75729f3dbd0c957f68519615c58a579d94acbdd7
Ghandi-dev <scoutghandi31@gmail.com>
2024-06-24 14:48:00 +0700

    fifth

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 
 const db = new Sequelize("db_koperasi2", "root", "", {
   host: "localhost",
   dialect: "mysql",
+  dialectModule: mysql2,
+  benchmark: true,
 });
 
 export default db;
